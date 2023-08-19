1a6897330a6aea1f4cf79198135987a7a9f967d4
Nicolas Smith <62912470+NickSmithSoftware@users.noreply.github.com>
2023-08-18 20:37:53 -0500

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,61 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 export default function Home() {
+  const [probabilities, setProbabilities] = useState('');
+  const [endPoint, setEndPoint] = useState('');
+
+  const parseProbability = (prob_str) => {
+    if (prob_str.includes("/")) {
+      const [numerator, denominator] = prob_str.split("/");
+      return parseFloat(numerator) / parseFloat(denominator);
+    } else {
+      return parseFloat(prob_str);
+    }
+  }
+
+  const calculatePercentileChance = (prob, kills) => {
+    return 1 - Math.pow((1 - prob), kills);
+  }
+
+  const handleChangeProb = (event) => {
+    setProbabilities(event.target.value);
+  }
+
+  const handleChangeEndpoint = (event) => {
+    setEndPoint(event.target.value);
+  }
+
+  let probValues = probabilities.split(",");
+  probValues = probValues.map((prob) => parseProbability(prob));
+
+  let kills = Array.from({ length: parseInt(endPoint) }, (_, i) => i + 1);
+
+  let chancesArrays = probValues.map((prob) => {
+    return kills.map((kill) => calculatePercentileChance(prob, kill));
+  });
+
+  const data = {
+    labels: kills,
+    datasets: probValues.map((prob, index) => ({
+      label: `P = ${prob}`,
+      data: chancesArrays[index],
+      fill: false,
+      borderColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+    })),
+  };
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div className="App">
+        <label>
+          Enter drop probabilities (comma separated):
+          <input type="text" onChange={handleChangeProb} />
+        </label>
+        <label>
+          Enter end point for number of kills:
+          <input type="text" onChange={handleChangeEndpoint} />
+        </label>
+        <Line data={data} />
     </div>
   )
 }
